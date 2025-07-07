548c2d78ee53650c92360b8d6318af4b231c2ee7
amin-kian <a.kianfard80@gmail.com>
2025-07-07 11:43:28 +0330

    Remove next.config.js from tracking for security

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    env: {
-        mongodb_username: "akianfard80",
-        mongodb_password: "Qca1kOYGPPMpMOJi",
-        mongodb_clustername: "cluster0",
-        mongodb_database: "my-blog",
-    }
-}
